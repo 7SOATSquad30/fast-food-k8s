e47b86efdb4a29c5a74f4bdfa31495818af6f790
Murilo Kakazu <yuri.kakazu@gmail.com>
2024-11-20 23:39:55 -0300

    build: deploy to aws academy

--- cluster.tf
+++ cluster.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "aws-fastfood-terraform-tfstate"
+    bucket = "aws-fastfood-fiap-terraform-tfstate"
     key    = "fast-food-eks/terraform.tfstate"
     region = "us-east-1"
   }
@@ -26,68 +26,77 @@ data "aws_ssm_parameter" "subnet_3" {
   name = "/rds/subnet_3"
 }
 
-resource "aws_iam_role" "eks_cluster_role" {
-  name = "eks-cluster-role"
+# default roles for aws academy
+data "aws_iam_role" "eks_cluster_role" {
+  name = "LabRole"
+}
+data "aws_iam_role" "eks_node_group_role" {
+  name = "LabRole"
+}
+#
+
+# resource "aws_iam_role" "eks_cluster_role" {
+#   name = "eks-cluster-role"
   
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "eks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "eks.amazonaws.com"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+#   role       = aws_iam_role.eks_cluster_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller_policy" {
-  role       = aws_iam_role.eks_cluster_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-}
+# resource "aws_iam_role_policy_attachment" "eks_vpc_resource_controller_policy" {
+#   role       = aws_iam_role.eks_cluster_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+# }
 
-resource "aws_iam_role" "eks_node_group_role" {
-  name = "eks-node-group-role"
+# resource "aws_iam_role" "eks_node_group_role" {
+#   name = "eks-node-group-role"
   
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         },
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+#   role       = aws_iam_role.eks_node_group_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-}
+# resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+#   role       = aws_iam_role.eks_node_group_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+# }
 
-resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry_readonly_policy" {
-  role       = aws_iam_role.eks_node_group_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-}
+# resource "aws_iam_role_policy_attachment" "eks_ec2_container_registry_readonly_policy" {
+#   role       = aws_iam_role.eks_node_group_role.name
+#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+# }
 
 resource "aws_eks_cluster" "eks_cluster" {
   name     = "basic-eks-cluster"
-  role_arn = aws_iam_role.eks_cluster_role.arn
+  role_arn = data.aws_iam_role.eks_cluster_role.arn
   version  = "1.25"
 
   vpc_config {
@@ -98,16 +107,21 @@ resource "aws_eks_cluster" "eks_cluster" {
 resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "eks-node-group"
-  node_role_arn   = aws_iam_role.eks_node_group_role.arn
+  node_role_arn   = data.aws_iam_role.eks_node_group_role.arn
   subnet_ids      = [data.aws_ssm_parameter.subnet_1.value, data.aws_ssm_parameter.subnet_2.value, data.aws_ssm_parameter.subnet_3.value]
 
   scaling_config {
     desired_size = 1
-    max_size     = 1
+    max_size     = 2
     min_size     = 1
   }
 
-  instance_types = ["t3.small"]
+  update_config {
+    max_unavailable_percentage = 50
+  }
+
+  instance_types = ["t2.small", "t3.small"]
+  capacity_type = "SPOT"
 
   depends_on = [
     aws_eks_cluster.eks_cluster
